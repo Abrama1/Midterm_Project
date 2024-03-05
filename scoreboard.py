49fc34beb8b65900d736eaae15c2bfa8c15a42db
Davit Abramishvili <david.abramishvili10@gmail.com>
2024-03-06 00:07:31 +0400

    Created functional scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 18, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            high_score = data.read()
+            self.high_score = int(high_score)
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score = {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", "w") as data:
+            data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score = {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
