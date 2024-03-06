208ba7d1705476e704cbe714ac740e5f957d1509
Davit Abramishvili <david.abramishvili10@gmail.com>
2024-03-06 17:28:56 +0400

    Added comments in main.py

--- Snakegame/main.py
+++ Snakegame/main.py
@@ -1,31 +1,39 @@
+# Import necessary modules and classes.
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
 
+# Set up the screen.
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.tracer(0)
+screen.tracer(0)  # Turn off animation.
 
+# Create snake, food, and scoreboard objects.
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# Listen for key presses and bind them to snake movement functions.
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+# Initialize game state variable.
 game_is_on = True
 
+# Main game loop.
 while game_is_on:
-
+    # Update screen.
     screen.update()
+    # Pause briefly to control game speed.
     time.sleep(0.1)
+    # Move the snake.
     snake.move()
 
     # Detect collision with food.
@@ -35,22 +43,20 @@ while game_is_on:
         scoreboard.increase_score()
 
     # Detect collision with wall.
-    if (snake.head.xcor() > 280
-            or snake.head.xcor() < -280
-            or snake.head.ycor() > 280
-            or snake.head.ycor() < -280):
-        game_is_on = False
-        scoreboard.reset()
-        scoreboard.game_over()
+    if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or
+            snake.head.ycor() > 280 or snake.head.ycor() < -280):
+        game_is_on = False  # End the game.
+        scoreboard.reset()  # Reset the scoreboard.
+        scoreboard.game_over()  # Display game over message.
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
-        if segment == snake.head:
+        if segment == snake.head:  # Skip the head segment.
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.reset()
-            scoreboard.game_over()
+            game_is_on = False  # End the game.
+            scoreboard.reset()  # Reset the scoreboard.
+            scoreboard.game_over()  # Display game over message.
 
-
+# Keep the screen open until clicked.
 screen.exitonclick()
